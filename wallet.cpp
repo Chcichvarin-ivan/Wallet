a46f183ef8918a1aeb853def94e485e06468e5ca
Ivan Chichvarin <ichichvarin@humanplus.ru>
2024-05-09 19:07:46 +0300

    optimal push and print functions

--- wallet.cpp
+++ wallet.cpp
@@ -2,10 +2,12 @@
  * @Author: Ivan Chichvarin ichichvarin@humanplus.ru
  * @Date: 2024-05-09 18:38:44
  * @LastEditors: Ivan Chichvarin ichichvarin@humanplus.ru
- * @LastEditTime: 2024-05-09 18:39:00
+ * @LastEditTime: 2024-05-09 19:05:46
  * @FilePath: /Wallet/wallet.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <vector>
 
@@ -23,11 +25,21 @@ public:
     }
 
     void PushCoin(int64_t value) {
-        // реализуйте метод добавления купюры или монеты
+        counts_[static_cast<size_t>(std::distance(nominals_.begin(),std::find(nominals_.begin(), nominals_.end(), value)))]++;
     }
 
     void PrintCoins(ostream& out) const {
-        // реализуйте метод печати доступных средств
+        auto countIt = counts_.begin();
+        auto nominalsIt = nominals_.begin();
+        while (nominalsIt != nominals_.end())
+        {
+            if(*countIt != 0){
+                out << *nominalsIt << ": " << *countIt << endl;
+            }
+            nominalsIt++;
+            countIt++;
+        }
+        
     }
 
 private:
@@ -39,8 +51,16 @@ ostream& operator<<(ostream& out, const MoneyBox& cash) {
     cash.PrintCoins(out);
     return out;
 }
-
+void Test(){
+    MoneyBox cash({1, 500, 10000});
+    cash.PushCoin(500);
+    cash.PushCoin(500);
+    cash.PushCoin(10000);
+    assert((cash.GetCounts() == vector<int>{0,2,1}));
+    cout << cash << endl;
+}
 int main() {
+    Test();
     MoneyBox cash({10, 50, 100, 200, 500, 1000, 2000, 5000});
 
     int times;
